1973b7206f3114fbac03782f0323b5ee46cc73b8
kalash0804 <167464704+kalash0804@users.noreply.github.com>
2025-07-14 12:58:26 +0530

    Update AppContext.jsx

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -31,7 +31,7 @@ const AppContextProvider = (props)=>{
     }
     const loadUserProfiledata = async()=>{
         try {
-            const {data} = await axios.get(backendUrl + '/api/user/get-profile', {headers: {token}})
+            const {data} = await axios.get(backendUrl + 'api/user/get-profile', {headers: {token}})
             if(data.success){
                 setUserData(data.userData)
             }else{
